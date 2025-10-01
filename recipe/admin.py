a3dcb5a64007774f6def0d3cb75704e9023d93b5
Мадина Рыспаева <madinator@Madinator.local>
2025-10-01 11:57:11 +0500

    add recipe creation, register Recipe model in admin panel

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from recipe.models import Recipe, RecipeIngredient
+
+
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "servings", "meal_type", "image", ]
+    search_fields = ("name",)
--- recipe/forms.py
+++ recipe/forms.py
@@ -1,5 +1,7 @@
+# forms.py
 from django import forms
 from django.forms import inlineformset_factory
+from recipe.choices import MealType
 from recipe.models import Recipe, RecipeIngredient
 
 
@@ -7,18 +9,45 @@ class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ["name", "servings", "description", "image", "meal_type"]
+        widgets = {
+            "image": forms.ClearableFileInput(
+                attrs={
+                    "class": "hidden",
+                    "accept": "image/*",
+                    "id": "id_image"
+                }
+            ),
+        }
+
+    meal_type = forms.ChoiceField(
+        choices=[("", "Выберите тип блюда")] + list(MealType.choices),
+        required=True,
+        label="Тип блюда"
+    )
 
 
 class RecipeIngredientForm(forms.ModelForm):
     class Meta:
         model = RecipeIngredient
         fields = ["product", "quantity", "unit"]
+        widgets = {
+            "product": forms.Select(attrs={"class": "form-input"}),
+            "quantity": forms.NumberInput(attrs={
+                "class": "form-input",
+                "min": "0",
+                "step": "any",
+                "inputmode": "decimal",  
+                "pattern": "[0-9]*([.,][0-9]+)?"
+            }),
+            "unit": forms.Select(attrs={"class": "form-input"}),
+        }
+
 
 RecipeIngredientFormSet = inlineformset_factory(
     Recipe,
     RecipeIngredient,
     form=RecipeIngredientForm,
     fields=["product", "quantity", "unit"],
-    extra=1,
+    extra=0,         
     can_delete=True,
 )
