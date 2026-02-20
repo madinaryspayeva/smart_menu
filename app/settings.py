66bf2e7eb38dc50a266dcdf3bf7ac82f7ddb5c20
Мадина Рыспаева <madinator@Madinator.local>
2026-02-20 13:01:18 +0500

    add dotenv path

--- app/settings.py
+++ app/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 import dj_database_url
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path="/code/.env.docker")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
